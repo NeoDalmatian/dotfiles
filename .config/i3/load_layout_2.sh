7359ee88b6bfcfddfec94485ec80eecf51769949
NeoDalmatian <aviskovic100@gmail.com>
2023-07-21 16:59:32 +0200

    Configurie workspace layouts

--- .config/i3/load_layout_2.sh
+++ .config/i3/load_layout_2.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 i3-msg "workspace 1; append_layout ~/.config/i3/workspace_2.json"
 
+sleep 0.1
+
 (alacritty &)
 
 sleep 0.1
 
-(alacritty -e ranger &)
+(alacritty -e ~/.config/i3/keep_open.sh ranger &)
 
 sleep 0.1
 
-(alacritty -e htop &)
+(alacritty -e ~/.config/i3/keep_open.sh /home/antonio/.nvm/versions/node/v16.16.0/bin/vtop &)
--- .config/i3/load_layout_5.sh
+++ .config/i3/load_layout_5.sh
@@ -3,4 +3,3 @@ i3-msg "workspace 5; append_layout ~/.config/i3/workspace_5.json"
 
 (code &)
 (alacritty &)
-(alacritty &)
